9f126876bdb95fe324a5095629c73a653a42c6cc
Administrator <407425690@qq.com>
2019-05-10 01:16:16 +0800

    评论+1

--- src/main/java/com/bth/lht/rest/comments/UserCommentController.java
+++ src/main/java/com/bth/lht/rest/comments/UserCommentController.java
@@ -1,5 +1,6 @@
 package com.bth.lht.rest.comments;
 
+import com.bth.lht.entity.project.MissionsEO;
 import com.bth.lht.entity.project.UserComments;
 import com.bth.lht.entity.user.UserEO;
 import com.bth.lht.request.UserCommentRequest.UserCommentRequest;
@@ -40,17 +41,32 @@ public class UserCommentController extends BaseController {
         UserEO userEO = userService.findByOpenid(openid);
         //评论响应实体
         UserComments userComments = new UserComments();
+
         if (userCommentRequest!=null){
-            userComments.setCommemtText(userCommentRequest.getCommentText());
-            userComments.setCommentLevel(userCommentRequest.getConnmentLevel());
-            userComments.setCommentUser(userEO);
+
             //找出完成任务的用户
             UserEO userEO1 = userService.findById(userCommentRequest.getUserid());
+            MissionsEO missionsEO = missionsService.get(userCommentRequest.getCommentMissionid());
+            UserComments  userComments1= userCommentService.findByAndCommentUserAndUserEO(userEO,userEO1);
+
+            if (userComments1!=null){
+                userComments1.setCommemtText(userCommentRequest.getCommentText());
+                userComments1.setCommentLevel(userCommentRequest.getConnmentLevel());
+                userCommentService.save(userComments1);
+                UserCommentResponse userCommentResponse = ModelMapperUtil.getStrictModelMapper().map(userComments1, new TypeToken<UserCommentResponse>() {
+                }.getType());
+                return successOne(userCommentResponse);
+            }else {
+                userComments.setCommemtText(userCommentRequest.getCommentText());
+                userComments.setCommentLevel(userCommentRequest.getConnmentLevel());
+            userComments.setMissionsEO(missionsEO);
             userComments.setUserEO(userEO1);
-            userComments.setMissionsEO(missionsService.get(userCommentRequest.getCommentMissionid()));
+
+            userComments.setCommentUser(userEO);
             userComments.setStatus("done");
             userComments.setCreateDate(new Date());
             userCommentService.save(userComments);
+        }
         }
 
         UserCommentResponse userCommentResponse = ModelMapperUtil.getStrictModelMapper().map(userComments, new TypeToken<UserCommentResponse>() {
